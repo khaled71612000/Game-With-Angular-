0dd733a30c2f02465392d89f705ff4d7ad6a9d63
Khaled Zakaria <khaled71612000@gmail.com>
2022-06-29 00:58:40 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { lorem } from "faker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'game-type';
+  randomText= lorem.sentence();
+  enteredText = '';
+
+  onInput(value:string) {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    // return of this function will be class name of span elemnt
+    if (!enteredLetter){
+      return 'pending';
+    }
+
+    if (enteredLetter === randomLetter) {
+      return 'correct';
+    }
+    else {
+      return 'incorrect'
+    }
+
+  }
+
 }
